6fe6ebb5fa3013e4bb4a5b6e35334ff90be875c0
Micael Gomez <mikael85@gmail.com>
2020-07-22 08:37:40 -0300

    [ADD] uom_model reference for 9.0

--- models/versions.py
+++ models/versions.py
@@ -7,7 +7,10 @@ _logger = logging.getLogger(__name__)
 # Odoo version 13.0
 
 # Odoo 12.0 -> Odoo 13.0
-uom_model = "uom.uom"
+# uom_model = "uom.uom"
+
+# Odoo 12.0 -> Odoo 9.0
+uom_model = "product.uom"
 
 # Odoo 12.0 -> Odoo 13.0
 prod_att_line = "product.template.attribute.line"
